c46c48e7e1db764c19b733737dd1297e19fbb9cf
Israel Calvete <icalvete@gmail.com>
2018-05-22 09:32:37 +0200

    Fix.

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,15 +9,15 @@ class geoip2::install {
   }
 
   exec { 'download_geoip2_city_database':
-    command  => "/usr/bin/curl -s http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz | tar xvfz - -C ${geoip2::root_path} --wildcards --no-anchored --strip-components 1 '*mmdb",
+    command  => "/usr/bin/curl -s http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz | tar xvfz - -C ${geoip2::root_path} --wildcards --no-anchored --strip-components 1 '*mmdb'",
     user     => 'root',
     provider => 'shell',
-    require  => File['geoip_root_path'],
+    require  => File['geoip2_root_path'],
     unless   => "/usr/bin/test -d ${geoip2::root_path}"
   }
 
   cron { 'update__geoip2_database':
-    command => "/usr/bin/flock /tmp/update_geoip2_database /usr/bin/curl -s http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz | tar xvfz - -C ${geoip2::root_path} --wildcards --no-anchored --strip-components 1 '*mmdb",
+    command => "/usr/bin/flock /tmp/update_geoip2_database /usr/bin/curl -s http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz | tar xvfz - -C ${geoip2::root_path} --wildcards --no-anchored --strip-components 1 '*mmdb'",
     user    => 'root',
     hour    => '0',
     minute  => '0',
